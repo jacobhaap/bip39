17f9e5cfaba51e0214aee2e25810dfb460ac1098
Jacob Haap <git@j3.cx>
2024-10-05 02:27:38 +0200

    Use byte array instead of binary in getChecksum

--- src/getChecksum.js
+++ src/getChecksum.js
@@ -3,15 +3,16 @@ const crypto = require('crypto');
 function getChecksum(entropy) {
     if (!entropy) {
         throw new Error(`Parameter 'entropy' is required.`);
-    } if (entropy.length < 128 || entropy.length > 256 || entropy.length % 32 !== 0) {
-        throw new Error(`Invalid Entropy Length '${entropy.length}'. Must be between 128 and 256 bits, and divisible by 32.`);
+    } if (entropy.length < 16 || entropy.length > 32 || entropy.length % 4 !== 0) {
+        throw new Error(`Invalid Entropy Length '${entropy.length * 8}' bits. Must be between 128 and 256 bits, and divisible by 32.`);
     }
 
-    const hash = crypto.createHash('sha256').update(entropy, 'binary').digest('hex');
-    const checksumLength = entropy.length / 32;
-    const checksum = parseInt(hash, 16).toString(2).padStart(256, '0').slice(0, checksumLength);
+    const hash = crypto.createHash('sha256').update(entropy).digest();
+    const checksumLength = entropy.length * 8 / 32;
+    const checksum = parseInt(hash.toString('hex'), 16).toString(2).padStart(256, '0').slice(0, checksumLength);
 
-    return entropy + checksum;
+    const entropyBinary = Array.from(entropy).map(byte => byte.toString(2).padStart(8, '0')).join('');
+    return entropyBinary + checksum;
 }
 
 module.exports = getChecksum;
